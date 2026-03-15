02e154d79020fdfa92a02702cfc1c85bdf922e3e
AmanDhole147 <vickeydhole@gmail.com>
2026-03-15 16:11:40 +0530

    portfolio deploy

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,10 @@ const basePath = process.env.NODE_ENV === "production" ? "/Resume-Nextjs" : "";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: basePath,
+  assetPrefix: basePath,
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
   env: {
     SITE_URL: process.env.SITE_URL,
